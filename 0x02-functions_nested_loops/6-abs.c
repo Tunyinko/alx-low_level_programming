4b5cdffaf14f19e81ba686c30c5803d6ecac7d96
Tunyinko <daniel.tunyinko@ashesi.edu.gh>
2023-03-03 11:17:02 -0800

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - Checks for absolute value
+ *
+ * @a: Parameter to be checked
+ *
+ * Return: always a
+ */
+int _abs(int a)
+{
+	if (a < 0)
+		a = -(a);
+	else if (a >= 0)
+		a = a;
+	return (a);
+}
