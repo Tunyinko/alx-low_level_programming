79f2a1087b03fca966a78d153ee3e371bb3a937b
Tunyinko <daniel.tunyinko@ashesi.edu.gh>
2023-03-03 13:04:21 -0800

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - function to generate times table
+ *
+ * Return: always 0
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		for (j = 0 ; j <= 9 ; j++)
+		{
+			k = i * j;
+			if (j != 9)
+			{
+				if (k > 9)
+				{	_putchar(k / 10 + '0');
+					_putchar(k % 10 + '0');
+				}
+				else
+				{
+					if (j != 0)
+					{
+						_putchar(' ');
+						_putchar(k + '0');
+					}
+					else if (j == 0)
+						_putchar(k + '0');
+				}
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if (j == 9)
+			{
+				if (k > 9)
+				{
+					_putchar(k / 10 + '0');
+					_putchar(k % 10 + '0');
+				}
+				else
+				{
+					_putchar(k + '0');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
