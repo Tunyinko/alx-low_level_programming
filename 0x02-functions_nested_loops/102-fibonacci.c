187177ffd51fe7e15d68aed4aad98e01102c68de
Tunyinko <daniel.tunyinko@ashesi.edu.gh>
2023-03-04 08:12:19 -0800

    fib

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,15 +9,15 @@
 int main(void)
 {
 	int i;
-	long long fib[50];
+	long fib[50];
 
 	fib[0] = 1;
 	fib[1] = 2;
-	printf("%lld, %lld, ", fib[0], fib[1]);
+	printf("%ld, %ld, ", fib[0], fib[1]);
 	for (i = 2; i < 50 ; i++)
 	{
 		fib[i] = fib[i - 1] + fib[i - 2];
-		printf("%lld", fib[i]);
+		printf("%ld", fib[i]);
 		if (i < 49)
 			printf(", ");
 		else
