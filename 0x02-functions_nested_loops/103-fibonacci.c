e6892295fec7a7e5de6ef3d1a60e669c4e2c8a0f
Tunyinko <daniel.tunyinko@ashesi.edu.gh>
2023-03-04 08:28:33 -0800

    another

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -12,13 +12,14 @@ int main(void)
 	long fib[3] = {1, 2, 3};
 	long sum = 2;
 
-	while (fib[2] < 4000000)
+	while (fib[i] < 4000000)
 	{
 		fib[0] = fib[1];
 		fib[1] = fib[2];
 		fib[2] = fib[0] + fib[1];
-		if (fib[2] % 2 == 0)
-			sum = sum + fib[2];
+		if (fib[i] % 2 == 0)
+			sum = sum + fib[i];
+		i++;
 	}
 	printf("%ld", sum);
 	printf("\n");
