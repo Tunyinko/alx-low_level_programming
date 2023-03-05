47cafb0ba80c96ae612f58ebef4efd4f199e4734
Tunyinko <daniel.tunyinko@ashesi.edu.gh>
2023-03-04 16:57:07 -0800

    Printing more numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - function that prints most numbers
+ *
+ * Return: always 0
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		if ((i != 2) && (i != 4))
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
