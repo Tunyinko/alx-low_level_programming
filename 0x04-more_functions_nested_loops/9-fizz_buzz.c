979f851e36f8731f8b18691bc8efca55db387c74
Tunyinko <daniel.tunyinko@ashesi.edu.gh>
2023-03-05 10:45:26 -0800

    I have got the Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,14 +12,19 @@ int main(void)
 
 	for (i = 1 ; i <= 100 ; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
-		else if (i % 3 == 0)
-			printf("Fizz ");
-		else if (i % 5 == 0)
-			printf("Buzz ");
+		if (i == 100)
+			printf("Buzz");
 		else
-			printf("%d ", i);
+		{
+			if (i % 3 == 0 && i % 5 == 0)
+				printf("FizzBuzz ");
+			else if (i % 3 == 0)
+				printf("Fizz ");
+			else if (i % 5 == 0)
+				printf("Buzz ");
+			else
+				printf("%d ", i);
+		}
 	}
 	printf("\n");
 	return (0);
