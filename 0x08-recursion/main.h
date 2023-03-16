d52304ae509315e1df5d5c2dd043bdb9bed27829
Tunyinko <daniel.tunyinko@ashesi.edu.gh>
2023-03-15 17:10:17 -0700

    recursive reverse printing

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -23,5 +23,12 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+
 
 #endif /* MAIN_H */
